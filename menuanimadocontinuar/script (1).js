7878816be9445cb7c3e8007a53ba72f1ca2d94d8
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-02-18 13:59:06 -0300

    Add files via upload

---
+++ menuanimadocontinuar/script (1).js
@@ -0,0 +1,6 @@
+const botao = document.querySelector('.botao-menu')
+const menulateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const background = document.querySelector('.background')
+
+botao.addEventListener('click')
