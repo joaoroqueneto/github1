1261372839adffcb030b4f5bcc63e0eab1109dfd
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-02-18 20:26:53 -0300

    att

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
  projetos
 <a href="https://joaoroqueneto.github.io/github1/youtube/projeto/index.html">executar o projeto </a><br>
 <a href="https://joaoroqueneto.github.io/github1/login003/index.html">login003 </a>
+<a href="https://joaoroqueneto.github.io/github1/login003/index.html">login003 </a>
--- menuanimadocontinuar/script.js
+++ menuanimadocontinuar/script.js
@@ -3,4 +3,17 @@ const menulateral = document.querySelector('.menu-lateral')
 const conteudo = document.querySelector('.conteudo')
 const background = document.querySelector('.background')
 
-botao.addEventListener('click')
+botao.addEventListener('click',() => {
+    menulateral.classList.toggle('ativo')
+    botao.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+    background.classList.toggle('ativo')
+    
+} )
+background.addEventListener('click',() => {
+    menulateral.classList.remove('ativo')
+    botao.classList.remove('ativo')
+    conteudo.classList.remove('ativo')
+    background.classList.remove('ativo')
+    
+} )
